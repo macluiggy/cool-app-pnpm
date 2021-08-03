ebd19d53750c7acc613672330c0b382f5036f186
Luiggy Macias <ferrinluiggy@gmail.com>
2021-08-03 14:23:42 -0500

    djrjhhhhhhhrhhhhh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to recargar.
+          Edita <code>src/App.js</code> and save to recargar.
         </p>
         <a
           className="App-link"
